15feb445a7c4fc9ec8384b477908432a13a92def
LockerUp <loockerup@gmail.com>
2024-02-02 17:16:11 +0700

    finish-1

--- main.py
+++ main.py
@@ -13,19 +13,20 @@ dataset.drop(['Unnamed: 0'], axis=1, inplace=True)
 x = dataset['text']
 y = dataset['label']
 
+# разделение на обучающий и тестовый наборы
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+
 # преобразование текстовых данных в матрицу TF-IDF признаков
 tfidf_vectorizer = TfidfVectorizer()
-x_tfidf = tfidf_vectorizer.fit_transform(x)
-
-# разделение на обучающий и тестовый наборы
-xtrain, xtest, ytrain, ytest = train_test_split(x_tfidf, y, test_size=0.2, random_state=42)
+xtrain_tfidf = tfidf_vectorizer.fit_transform(xtrain)
+xtest_tfidf = tfidf_vectorizer.transform(xtest)
 
 # создание и обучение модели Passive Aggressive Classifier
 model = PassiveAggressiveClassifier()
-model.fit(xtrain, ytrain)
+model.fit(xtrain_tfidf, ytrain)
 
 # оценка производительности модели
-print('Точность: ', model.score(xtest, ytest))
+print('Точность: ', model.score(xtest_tfidf, ytest))
 
 # демонстрация работоспособности модели
 news_headline = 'US threatens to reimpose sanctions on Venezuelan oil sector'
@@ -33,7 +34,7 @@ data = tfidf_vectorizer.transform([news_headline])
 print(model.predict(data))
 
 # получаем предсказания модели на тестовом наборе
-pred = model.predict(xtest)
+pred = model.predict(xtest_tfidf)
 
 # вычисляем матрицу ошибок
 conf_matrix = confusion_matrix(ytest, pred)
